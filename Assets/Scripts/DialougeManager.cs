8c9db73a296814d08906e70eaa5d5a07fa9cb58c
Barrington H. Scott <barrington_scott@bloomfield.edu>
2019-04-28 17:26:37 -0400

    Major Changes made.

---
+++ Assets/Scripts/DialougeManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialougeManager: MonoBehaviour {
+
+    public GameObject DBox;
+    public Text DText;
+
+    public bool dialogActive;
+    
+    void Start() {
+
+    }
+
+    void Update() 
+    {
+        if (dialogActive && Input.GetKeyDown(KeyCode.S))
+        {
+            DBox.SetActive(false);
+            dialogActive = false;
+        }
+    }
+
+    public void Show (string dialouge)
+    {
+        dialogActive = true;
+        DBox.SetActive(true);
+        DText.text = dialouge;
+    }
+}
---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Inventory : MonoBehaviour
+{
+    public static int OrbsCount;
+
+    void Start()
+    {
+        OrbsCount = 0;
+    }
+}
