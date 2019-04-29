525b97ee59625834f8d853bcdd861f47e719dae4
ArrowOmega <38959592+ArrowOmega@users.noreply.github.com>
2019-04-29 13:40:21 -0400

    Fixed the Orb counter and it resets when the player presses the start button in the menu. All the button works.

--- Assets/Scripts/Title.cs
+++ Assets/Scripts/Title.cs
@@ -1,18 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class Title : MonoBehaviour
 {
+    public string start;
+    public string title;
+    public string instructions;
 
-
-    // Update is called once per frame
-    void Update()
+    public void NewGame()
     {
-        if (Input.GetKeyDown(KeyCode.Return))
-        {
-            SceneManager.LoadScene("Instructions");
-        }
+        SceneManager.LoadScene(start);
+
+        PlayerPrefs.SetInt("CurrentPlayerScore", 0);
+
+    }
+
+    public void Controls()
+    {
+        SceneManager.LoadScene(instructions);
+    }
+
+    public void TitleMenu()
+    {
+        SceneManager.LoadScene(title);
     }
 }
