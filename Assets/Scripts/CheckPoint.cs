8b96eaa48f3f1af849cf1cf528c37533b164c61e
ArrowOmega <38959592+ArrowOmega@users.noreply.github.com>
2019-05-01 20:20:55 -0400

    Added the heckPoint system and made other fixes
    Including kill script, fixed the reset counter, added a level manager.

---
+++ Assets/Scripts/CheckPoint.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPoint : MonoBehaviour
+{
+    public LevelManager levelManager;
+
+    void Start()
+    {
+        levelManager = FindObjectOfType<LevelManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.name == "Turtle Player")
+        {
+            levelManager.currentCheckPoint = gameObject;
+            Debug.Log("Activated Checkpont" + transform.position);
+
+        }
+    }
+}
---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    private Moving player;
+    public GameObject currentCheckPoint;
+    public float respawnDelay;
+    private float gravityStore;
+    private CameraController camera;
+
+    public void Start()
+    {
+        player = FindObjectOfType<Moving>();
+
+        camera = FindObjectOfType<CameraController>();
+    }
+    public void RespawnPlayer()
+    {
+        StartCoroutine("RespawnPlayerCo");
+    }
+
+    public IEnumerator RespawnPlayerCo()
+    {
+        player.enabled = false;
+        player.GetComponent<Renderer>().enabled = false;
+
+        camera.isFollowing = false;
+
+        Debug.Log("Player Respawn");
+        yield return new WaitForSeconds(respawnDelay);
+
+        player.transform.position = currentCheckPoint.transform.position;
+
+        camera.isFollowing = true;
+
+        player.enabled = true;
+        player.GetComponent<Renderer>().enabled = true;
+    }
+}
