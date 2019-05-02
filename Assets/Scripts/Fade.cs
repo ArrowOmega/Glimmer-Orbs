9f05058998a11ae3d4a55c223c0b5b36f6cf49f9
Barrington H. Scott <barrington_scott@bloomfield.edu>
2019-05-01 21:08:40 -0400

    Attempting fade Script with sound and polish.

---
+++ Assets/Scripts/Fade.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Fade : MonoBehaviour
+{
+    public int index;
+
+    public Image black;
+    public Animator anim;
+
+    public AudioSource FadeSource;
+    public AudioClip transition;
+
+    [Range(0.0f, 1.0f)]
+    public float FadeVolume;
+
+    private void OnTriggerEnter(Collider collision)
+    {
+       if (collision.tag == "Player")
+        {
+            collision.GetComponent<Rigidbody>().useGravity = false;
+            FadeSource.PlayOneShot(transition, FadeVolume);
+            StartCoroutine(Fading());
+        }
+    }
+
+IEnumerator Fading()
+
+    {
+        Debug.Log("hello");
+        anim.SetBool("Fade", true);
+        yield return new WaitUntil(() => { return black.color.a == 1f; });
+        SceneManager.LoadScene(index);
+    }
+}
